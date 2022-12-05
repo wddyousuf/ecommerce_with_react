8449ff062b1b74a3d52ab6a230ab8ca075b5d08e
wddyousuf <saikatyousuf@gmail.com>
2022-12-06 00:49:23 +0600

    REACT ECOMMERCE

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import data from './data';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+function App() {
+  return (
+    <BrowserRouter>
+      <div>
+        <header>
+          <Link to="/">Amazona</Link>
+        </header>
+        <main>
+          <Routes>
+            <Route path="/" element={<HomeScreen />}></Route>
+            <Route path="/product/:slug" element={<ProductScreen />}></Route>
+          </Routes>
+        </main>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
